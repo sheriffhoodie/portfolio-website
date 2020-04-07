fdc34933f185887e649991ad11abbcd914b5a2bd
max.currier <maxwell.currier@syneoshealth.com>
2020-04-07 18:00:05 -0400

    init base styles, working button animation

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,1 +1,47 @@
-console.log('animations here');
+const GREETINGS = [
+  'Bonjour!',
+  'Buongiorno!',
+  'Hola!',
+  '안녕하세요!',
+  'Guten Tag!',
+  'こんにちは!',
+  'Hi!'
+]
+
+function rotateGreeting() {
+  var i = 0;
+  setInterval(function() {
+    document.querySelector('.language-toggle').innerHTML = GREETINGS[i];
+    i === GREETINGS.length - 1 ? i = 0 : i++;
+  }, 1500);
+}
+
+function animateButton(ctx, event) {
+  var btnSpan = ctx.querySelector('span');
+
+  var relX = event.pageX - ctx.offsetLeft,
+  relY = event.pageY - ctx.offsetTop;
+
+  gsap.to(btnSpan, { duration: 0.1, top: relY, left: relX });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // rotateGreeting();
+
+  // Apply Event Listeners for Button Animations
+  var BUTTONS = document.querySelectorAll('.button');
+
+  for (var i = 0; i < BUTTONS.length; i++) {
+
+    BUTTONS[i].addEventListener('mouseenter', function(e) {
+      animateButton(this, e);
+    });
+
+    BUTTONS[i].addEventListener('mouseleave', function(e) {
+      animateButton(this, e);
+    });
+  }
+});
+
+var tl = gsap.timeline({paused: true});
+console.log(tl);
--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
-console.log('here');
+
+var resumeBtn = document.querySelector('.button[data-target="resume"]');
+resumeBtn.addEventListener('click', function() {
+  window.open('assets/MC-SD-resume.pdf');
+})

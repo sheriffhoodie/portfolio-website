40e28110652dba6f0689f043ed90f66cb63855d2
max.currier <maxwell.currier@syneoshealth.com>
2020-04-09 20:07:27 -0400

    working nav functionality

--- src/js/-main.js
+++ src/js/-main.js
@@ -3,3 +3,11 @@ var resumeBtn = document.querySelector('.button[data-target="resume"]');
 resumeBtn.addEventListener('click', function() {
   window.open('assets/MC-SD-resume.pdf');
 })
+
+function deactivate(el) {
+  el.classList.remove('active');
+}
+
+function activate(el) {
+  el.classList.add('active');
+}
---
+++ src/js/nav.js
@@ -0,0 +1,16 @@
+// Nav Functionality
+var navLinks = document.querySelectorAll('[data-link]');
+var contentPages = document.querySelectorAll('.content-page');
+var contentPagesArray = Array.from(contentPages);
+var pageMatch, currentPage;
+
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', function() {
+
+    pageMatch = contentPagesArray.filter(page => page.id === this.dataset.link)[0];
+    currentPage = document.querySelector('.content-page.active');
+
+    deactivate(currentPage);
+    activate(pageMatch);
+  });
+}
